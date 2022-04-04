bb529170fdf081e1e6595e6ca5bd4003608865db
Rukesh Shrestha <rukesh.shrestha11@gmail.com>
2022-04-04 20:25:12 +0545

    update Project 1

---
+++ src/component/app/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "../header/Header";
+import Footer from "../footer/Footer";
+import FactsReact from "../facts/Facts";
+import Learn from "../learn/Learn";
+
+
+function App() {
+  return (  
+    <> 
+    <div>
+      <Header/>
+      <FactsReact/>
+      <Learn/>
+      <Footer/>
+    </div>   
+     
+    </>
+
+    
+  );
+}
+
+export default App;
